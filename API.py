6f10d77b66433f80abe0256ff8b1da8d60012643
Ondřej Kavan <ondrej.kavan@gjkt.eu>
2023-11-19 12:01:35 +0100

    Update - API file, new functions

---
+++ API.py
@@ -0,0 +1,3 @@
+import json
+from flask import *
+import data
--- Image Source Processing.py
+++ Image Source Processing.py
@@ -1,8 +1,13 @@
 import data
 
-class Create_Image_Source:      #Function that creates image source key in "Image_src" dictionary in "data.py" script
+class Create_Image_Source:      #Class that creates image source key in "Image_src" dictionary in "data.py" script
     
     def __init__(self, key, value):
         if key not in data.Image_src:       #Checks if key exists 
             data.Image_src[key] = value
-    
+
+class Remove_Image_Source:      #Class that deletes existing image source from "Image_src" dictionary in "data.py"
+
+    def __init__(self, key):
+        if key in data.Image_src:       #Checks if key exists (prevents key error)
+            del data.Image_src[key]
